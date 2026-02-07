d9574ff900816a2a43d29d073731de25eb9990fb
Isonski <elicasotirova6@gmail.com>
2026-02-07 15:37:43 +0200

    Added Data, Services and Web projects

---
+++ TicketFlow.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TicketFlow.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
